c2a17498c051d0014f00dfec59f7c9ccd907f6af
sanrajpal <34855803+sanrajpal@users.noreply.github.com>
2025-02-01 20:18:20 +0530

    fist commit

---
+++ util/random.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var randomSource *rand.Rand
+
+func init() {
+	randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + randomSource.Int63n(max-min+1)
+}
+
+// RandomString generates a random integer between min and max
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(10, 25000000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
